b35422e802c75b0b2cf04735fa47d868b912d237
akihiko.kigure <akihiko.kigure@gmail.com>
2025-12-14 00:10:28 +0900

    chore: create library project structure

---
+++ packages/web-serial-rxjs/src/lib/web-serial-rxjs.spec.ts
@@ -0,0 +1,7 @@
+import { webSerialRxjs } from './web-serial-rxjs';
+
+describe('webSerialRxjs', () => {
+  it('should work', () => {
+    expect(webSerialRxjs()).toEqual('web-serial-rxjs');
+  });
+});
---
+++ packages/web-serial-rxjs/src/lib/web-serial-rxjs.ts
@@ -0,0 +1,3 @@
+export function webSerialRxjs(): string {
+  return 'web-serial-rxjs';
+}
