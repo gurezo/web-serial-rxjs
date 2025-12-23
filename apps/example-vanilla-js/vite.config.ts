c58cc22407b8d204245ec2e6c9409794bb912dd1
akihiko.kigure <akihiko.kigure@gmail.com>
2025-12-23 22:46:28 +0900

    refactor(apps): move vanilla-js to example-vanilla-js

---
+++ apps/example-vanilla-js/vite.config.ts
@@ -0,0 +1,48 @@
+/// <reference types='vitest' />
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const workspaceRoot = resolve(__dirname, '../../../');
+
+export default defineConfig(() => ({
+  root: import.meta.dirname,
+  cacheDir: '../../../node_modules/.vite/apps/example-vanilla-js',
+  plugins: [nxViteTsPaths()],
+  server: {
+    port: 4200,
+    host: true,
+    strictPort: false,
+    fs: {
+      // Allow serving files from workspace root for monorepo setup
+      allow: [workspaceRoot],
+    },
+  },
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+  build: {
+    outDir: '../../../dist/apps/example-vanilla-js',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    emptyOutDir: true,
+  },
+  test: {
+    name: 'example-vanilla-js',
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../../coverage/apps/example-vanilla-js',
+      provider: 'v8' as const,
+    },
+  },
+}));
